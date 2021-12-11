2006ae380ce3347aab573f83cd7a71a1a6b303fb
Eric <ericpoliveira@gmail.com>
2021-12-10 22:14:09 -0300

    intoducao_poo

---
+++ poo/Aluno.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace poo
+{
+    class Aluno
+    {
+        //Atributos
+        public string nome;
+        public double nota1, nota2;
+
+        //Métodos
+
+        //Média
+        public double media()
+        {
+            return (nota1 + nota2) / 2;
+        }
+        //Situação
+        public string situacao(double media)
+        {
+            return media >= 7 ? "Aprovado" : "Reprovado";
+        }
+        //Mensagem
+        public void mensagem()
+        {
+            //Obter a média
+            double obterMedia = media();
+
+            //Obter a situação
+            string obterSituacao = situacao(obterMedia);
+
+            //Mensagem
+            Console.WriteLine(nome + " está " + obterSituacao + " com média " + obterMedia);
+        }
+    }
+}
---
+++ poo/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace poo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            
+            //Instanciar objeto da classe aluno
+            Aluno a = new Aluno();
+            a.nome = "Eric";
+            a.nota1 = 8;
+            a.nota2 = 10;
+            a.mensagem();
+
+            Console.ReadKey();
+        }
+    }
+}
